693640d0226c3e5c79750d0d55d5a38f6a1829eb
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-07-05 11:47:14 +0100

    MongoDb Setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-console.log("Hello Solomon!");
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log("Server running at http://localhost:8080/");
+});
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database connected successfully"))
+  .catch((error: Error) => {
+    console.log({
+      error: error,
+      message: "There is an error connecting with the Mongo Database",
+    });
+  });
